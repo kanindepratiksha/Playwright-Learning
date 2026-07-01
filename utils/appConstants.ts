74f1721c178b39476179a7a775e5f45bd6e6c939
KanindePratiksha2 <pratiksha.kaninde@theprovenancechain.com>
2026-07-01 21:46:46 +0530

    remove url from appconstant

--- utils/appConstants.ts
+++ utils/appConstants.ts
@@ -1,14 +1,6 @@
 export const testData = {
 
     // ==========================================
-    // Application URLs
-    // ==========================================
-    sauceDemoUrl: 'https://www.saucedemo.com/',
-    framesUrl: 'https://demoqa.com/frames',
-    alertsUrl: 'https://demoqa.com/alerts',
-    browserWindowsUrl: 'https://demoqa.com/browser-windows',
-
-    // ==========================================
     // Page Titles
     // ==========================================
     productPageTitle: 'Products',
