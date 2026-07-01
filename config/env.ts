22650d7666a475bb95bc3fa063a67f27896b1437
KanindePratiksha2 <pratiksha.kaninde@theprovenancechain.com>
2026-07-02 00:02:53 +0530

    Remove Browser Windows feature from main

--- config/env.ts
+++ config/env.ts
@@ -6,6 +6,5 @@ export const config = {
     sauceDemoUrl: 'https://www.saucedemo.com/',
     framesUrl: 'https://demoqa.com/frames',
     alertsUrl: 'https://demoqa.com/alerts',
-    browserWindowsUrl: 'https://demoqa.com/browser-windows'
 
 };
