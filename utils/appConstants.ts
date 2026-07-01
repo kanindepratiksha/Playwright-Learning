c57b624d616673abdbc5a0135f469b0bc64b1b48
KanindePratiksha2 <pratiksha.kaninde@theprovenancechain.com>
2026-07-01 23:52:34 +0530

    Restore appConstants.ts

---
+++ utils/appConstants.ts
@@ -0,0 +1,45 @@
+export const testData = {
+
+    // ==========================================
+    // Buttons
+    // ==========================================
+    loginButton: 'Login',
+    logoutButton: 'Logout',
+
+    // ==========================================
+    // Page Titles
+    // ==========================================
+    productPageTitle: 'Products',
+    cartPageTitle: 'Your Cart',
+
+    // ==========================================
+    // Products
+    // ==========================================
+    product1: 'Sauce Labs Backpack',
+    product2: 'Sauce Labs Bike Light',
+
+    productNameAZ: 'Sauce Labs Backpack',
+    productNameZA: 'Test.allTheThings() T-Shirt (Red)',
+
+    lowPrice: '$7.99',
+    highPrice: '$49.99',
+    highPriceProduct: 'Sauce Labs Fleece Jacket',
+
+    // ==========================================
+    // Frames
+    // ==========================================
+    frameHeading: 'This is a sample page',
+
+    // ==========================================
+    // Alerts
+    // ==========================================
+    promptText: 'Pratiksha',
+    confirmResult: 'You selected Ok',
+    promptResult: 'You entered Pratiksha',
+
+    // ==========================================
+    // Browser Windows
+    // ==========================================
+    newTabHeading: 'This is a sample page'
+
+};
